ee029abce64d674ecc946ffa3abf47a07a2ddc84
Wang Teng <wt0318@outlook.com>
2023-07-26 23:11:04 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({extended: true}));
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema = {email: String, password: String};
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", function (req, res) {
+    res.render("home");
+});
+
+app.get("/login", function (req, res) {
+    res.render("login");
+});
+app.post("/login", function (req, res) {
+    const email = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({"email": email})
+        .then(function (foundItem) {
+            if (foundItem.password === password) {
+                res.render("secrets");
+            }
+        })
+        .catch(function (err) {
+            console.log(err);
+        })
+})
+app.get("/register", function (req, res) {
+    res.render("register");
+});
+app.post("/register", function (req, res) {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password,
+    });
+    newUser.save()
+        .then(function () {
+            res.render("secrets");
+        })
+        .catch(function (err) {
+            res.send(err);
+        })
+})
+
+
+app.listen(3000, function () {
+    console.log("Successfully running on the port 3000.");
+});
